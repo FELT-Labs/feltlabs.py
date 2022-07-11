0bcd9ed24e3e756d278766f654216379b81b4819
Breta01 <breta.hajek@gmail.com>
2022-07-11 16:20:40 +0200

    Update version

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ requirements = parse_requirements(PATH / "requirements.txt")
 
 setup(
     name="feltlabs",
-    version="0.2.3",
+    version="0.2.4",
     packages=find_packages(),
     maintainer="FELT Labs",
     maintainer_email="info@bretahajek.com",
